5274154088ae0c7b3134bd39f7e4595989153958
MD. Juwel Rana <juwelrana3426@gmail.com>
2025-10-29 23:09:02 +0600

    Fix URL formatting in Google login handler

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,8 +23,8 @@ export default function LoginPage() {
     try {
       await account.createOAuth2Session(
         OAuthProvider.Google,
-        https://revisifybd.netlify.app/dashboard`,
-        `https://revisifybd.netlify.app/login`,
+        https://revisifybd.netlify.app/dashboard,
+        `https://revisifybd.netlify.app/login,
       );
       
     } catch (error) {
